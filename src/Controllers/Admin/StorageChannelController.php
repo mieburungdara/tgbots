2a21d61ab9b98d046b3fb00a75d3da5ef24c2640
Rerek <rereitureza@gmail.com>
2025-08-30 03:33:57 +0800

    Update StorageChannelController.php

--- src/Controllers/Admin/StorageChannelController.php
+++ src/Controllers/Admin/StorageChannelController.php
@@ -16,8 +16,6 @@ class StorageChannelController extends BaseController {
         parent::__construct();
         $pdo = get_db_connection();
         if (!$pdo) {
-            // Melempar exception akan menghentikan eksekusi dan dapat ditangkap oleh error handler global.
-            // Ini lebih baik daripada fatal error karena tipe argumen yang salah.
             throw new \RuntimeException("Koneksi database gagal.");
         }
         $this->channelRepo = new PrivateChannelRepository($pdo);
