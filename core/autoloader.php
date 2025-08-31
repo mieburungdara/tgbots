c6279476f4321474c7ca2ad691fdc8b6cb824ae0
rerek <rereitureza@gmail.com>
2025-08-31 21:48:41 +0000

    🐛 8

--- core/autoloader.php
+++ core/autoloader.php
@@ -4,9 +4,20 @@ require_once __DIR__ . '/../config.php';
 require_once __DIR__ . '/helpers.php';
 
 spl_autoload_register(function ($class) {
+    $prefix = 'TGBot\\';
+    if (strncmp($prefix, $class, strlen($prefix)) !== 0) {
+        return;
+    }
+
+    $relative_class = substr($class, strlen($prefix));
     $base_dir = __DIR__ . '/../';
 
-    $file = $base_dir . str_replace('\\', '/', $class) . '.php';
+    // Map TGBot\Router to core/Router.php
+    if ($relative_class === 'Router') {
+        $file = $base_dir . 'core/Router.php';
+    } else {
+        $file = $base_dir . 'src/' . str_replace('\\', '/', $relative_class) . '.php';
+    }
 
     if (file_exists($file)) {
         require_once $file;
