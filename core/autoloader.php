9b74c10e3ef806319e5feaabd6071bc8f81edf81
rerek <rereitureza@gmail.com>
2025-08-31 21:46:27 +0000

    🐛 7

--- core/autoloader.php
+++ core/autoloader.php
@@ -6,7 +6,7 @@ require_once __DIR__ . '/helpers.php';
 spl_autoload_register(function ($class) {
     $base_dir = __DIR__ . '/../';
 
-    $file = $base_dir . str_replace('''', '/', $class) . '.php';
+    $file = $base_dir . str_replace('\\', '/', $class) . '.php';
 
     if (file_exists($file)) {
         require_once $file;
